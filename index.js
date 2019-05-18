7b4bf2ead3418c33d79818a98aa5f1ccd7f93477
wailunlim <wailun@u.nus.edu>
2019-05-19 02:14:12 +0800

    Add chat widget

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const ask = require("./controller").ask;
 const bodyParser = require("body-parser");
 
+const cors = require("cors");
+
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(cors());
 
 app.get("/", express.static("public"));
 app.post("/ask", ask);
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(9000, () => console.log("Listening on port 9000"));
